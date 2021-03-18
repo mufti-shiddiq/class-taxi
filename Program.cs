ab2dc50c436cd918281e3334064fd87f371b195f
mufti-shiddiq <mufti@students.amikom.ac.id>
2021-03-18 22:20:17 +0700

    Menambahkan file Program dan file class taxi

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// membuat objek Taxi
+			Taxi taxi = new Taxi();
+
+			// mengeset nilai properties
+			taxi.DriverName = "Jono";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			// memanggil method
+			taxi.TaxiInfo();
+			taxi.PickUpPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Taxi
+{
+    public class Taxi
+    {
+		// properties
+		public string DriverName { get; set; }
+		public bool OnDuty { get; set; }
+		public int NumPassenger { get; set; }
+
+		// method
+		public void TaxiInfo()
+		{
+			Console.WriteLine("Driver Name         : {0}", DriverName);
+			Console.WriteLine("On Duty             : {0}", OnDuty);
+			Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+		}
+
+		public void PickUpPassenger()
+		{
+			Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+		}
+
+		public void DropOffPassenger()
+		{
+			Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+		}
+
+	}
+}
